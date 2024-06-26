3b0ba819317081e1e5c3165e55efae41866c0782
Joao-Pedro-P-Holanda <joaopedroph.dev@gmail.com>
2024-06-26 11:44:27 -0300

    feat: implementing DatabaseConnection class
    removing unused dependencies and plugin
    
    adding method getInstance with dotenv dependency
    
    changing dotenv attribute of DatabaseConnection to not be static
    
    using mocks instead of managing environment variables

--- src/main/java/br/ufc/quixada/dao/DatabaseConnection.java
+++ src/main/java/br/ufc/quixada/dao/DatabaseConnection.java
@@ -1,28 +1,55 @@
 package br.ufc.quixada.dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class DatabaseConnection {
-	private static Dotenv dotenv = Dotenv.load();
+	private Dotenv dotenv;
 
-	private static final String DATABASE_URL = dotenv.get("DATABASE_URL");
+	private static String DATABASE_URL;
 
+	private static DatabaseConnection instance;
 	private static Connection connection;
 
-	private DatabaseConnection() {
+	private DatabaseConnection() throws SQLException {
+		dotenv = Dotenv.load();
+		connect();
+	}
+
+	private DatabaseConnection(Dotenv dotenv) throws SQLException {
+		this.dotenv = dotenv;
+		connect();
+	}
+
+	private void connect() throws SQLException {
+		DATABASE_URL = dotenv.get("DATABASE_URL");
+		connection = DriverManager.getConnection(DATABASE_URL);
+		System.out.println("Connected to the database.");
 	}
 
 	public Connection getConnection() {
-		return null;
+		return connection;
 	}
 
-	public void disconnect() {
+	public void disconnect() throws SQLException {
+		connection.close();
 	}
 
-	public static DatabaseConnection getInstance() {
-		return null;
+	public static DatabaseConnection getInstance() throws SQLException {
+		if (instance == null) {
+			instance = new DatabaseConnection();
+		}
+		return instance;
+	}
+
+	public static DatabaseConnection getInstance(Dotenv dotenv) throws SQLException {
+		if (instance == null) {
+			instance = new DatabaseConnection(dotenv);
+		}
+		return instance;
 	}
 
 }
--- src/test/java/br/ufc/quixada/dao/TestDatabaseConnection.java
+++ src/test/java/br/ufc/quixada/dao/TestDatabaseConnection.java
@@ -5,43 +5,71 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.sql.SQLException;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junitpioneer.jupiter.SetEnvironmentVariable;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class TestDatabaseConnection {
+    private static Dotenv dotenv;
+
+    /**
+     * creating a mock Dotenv object to be injected into the DatabaseConnection
+     * getInstance method
+     */
+    @BeforeAll
+    public static void setUpAll() {
+        dotenv = mock(Dotenv.class);
+        when(dotenv.get("DATABASE_URL")).thenReturn("jdbc:sqlite::memory:");
+    }
+
+    /**
+     * resetting the instance field to null on each test, using reflection
+     * 
+     * @throws Exception
+     */
+    @BeforeEach
+    public void setUp() throws Exception {
+        // resetting the instance field to null on each test
+        setPrivateField(DatabaseConnection.class, DatabaseConnection.getInstance(dotenv), "instance", null);
+    }
 
     @Test
-    @SetEnvironmentVariable(key = "DATABASE_URL", value = "jdbc:sqlite:test_database.db")
-    void testGetInstanceWithValidDatabaseURL() {
-        DatabaseConnection databaseConnection = null;
-        assertDoesNotThrow(() -> DatabaseConnection.getInstance(),
+    void testGetInstanceWithValidDatabaseURL() throws Exception {
+        DatabaseConnection databaseConnection = DatabaseConnection.getInstance(dotenv);
+
+        assertDoesNotThrow(() -> DatabaseConnection.getInstance(dotenv),
                 "Getting instance should not throw an Exception with valid database URL");
         assertNotNull(databaseConnection, "DatabaseConnection instance should not be null");
     }
 
     @Test
-    @SetEnvironmentVariable(key = "DATABASE_URL", value = "invalid_database_url")
-    void testGetInstanceWithInvalidDatabaseURL() {
-        assertThrows(SQLException.class, () -> DatabaseConnection.getInstance(),
+    void testGetInstanceWithInvalidDatabaseURL() throws Exception {
+        Dotenv invalidDotenv = mock(Dotenv.class);
+        when(invalidDotenv.get("DATABASE_URL")).thenReturn("invalid_url");
+
+        assertThrows(SQLException.class, () -> DatabaseConnection.getInstance(invalidDotenv),
                 "Getting instance should throw an Exception with a invalid database URL");
+
     }
 
     @Test
-    @SetEnvironmentVariable(key = "DATABASE_URL", value = "jdbc:sqlite:test_database.db")
-    void testGetValidConnection() {
-        DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
+    void testGetValidConnection() throws Exception {
+        DatabaseConnection databaseConnection = DatabaseConnection.getInstance(dotenv);
 
         assertNotNull(databaseConnection);
         assertNotNull(databaseConnection.getConnection(), "Connection should not be null");
     }
 
     @Test
-    @SetEnvironmentVariable(key = "DATABASE_URL", value = "jdbc:sqlite:test_database.db")
-    void testDisconnectValidConnection() throws SQLException {
-        DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
+    void testDisconnectValidConnection() throws Exception {
+        DatabaseConnection databaseConnection = DatabaseConnection.getInstance(dotenv);
 
         assertNotNull(databaseConnection);
         assertDoesNotThrow(() -> databaseConnection.disconnect(),
@@ -51,12 +79,32 @@ public class TestDatabaseConnection {
     }
 
     @Test
-    void testGetInstanceReturnSameObjectOnRepeatedCall() {
-        DatabaseConnection databaseConnection1 = DatabaseConnection.getInstance();
-        DatabaseConnection databaseConnection2 = DatabaseConnection.getInstance();
+    void testGetInstanceReturnSameObjectOnRepeatedCall() throws Exception {
+        DatabaseConnection databaseConnection1 = DatabaseConnection.getInstance(dotenv);
+        DatabaseConnection databaseConnection2 = DatabaseConnection.getInstance(dotenv);
 
         assertNotNull(databaseConnection1);
         assertNotNull(databaseConnection2);
         assertSame(databaseConnection1, databaseConnection2, "Both instances should be the same");
     }
+
+    /**
+     * Reflection method to set a private field of a class to a new value.
+     * Reference:
+     * https://stackoverflow.com/questions/54819923/how-to-destroy-or-reset-singleton-instance-in-java
+     * 
+     * @param clazz class to set the field
+     * @param inst  instance of the class
+     * @param field
+     * @param value
+     * @throws Exception
+     */
+    public static void setPrivateField(Class<DatabaseConnection> clazz, Object inst, String field, Object value)
+            throws Exception {
+        java.lang.reflect.Field f = clazz.getDeclaredField(field);
+        f.setAccessible(true);
+        f.set(inst, value);
+        f.setAccessible(false);
+    }
+
 }
