39bd2ed4db3b65aa0faed2f952872e864d49ee83
Francisco Paulino Arruda Filho <fpaulinofilho04@gmail.com>
2024-08-25 15:20:35 -0300

    HistoricController

--- src/main/java/br/ufc/quixada/controller/HistoricController.java
+++ src/main/java/br/ufc/quixada/controller/HistoricController.java
@@ -1,11 +1,18 @@
 package br.ufc.quixada.controller;
 
-// import br.ufc.quixada.model.MatchHistory;
-// import br.ufc.quixada.dao.HistoricDAO;
 import br.ufc.quixada.util.SceneManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import br.ufc.quixada.model.MatchHistory;
+import br.ufc.quixada.model.Player;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import br.ufc.quixada.dao.MatchHistoryDAO;
 // import javafx.scene.layout.HBox;
 // import javafx.scene.control.Label;
 
@@ -19,26 +26,43 @@ public class HistoricController {
     @FXML
     private Button ButtonBackToHome;
 
-    // private HistoricDAO historicDAO;
+    private MatchHistoryDAO historicDAO;
 
     @FXML
-    public void initialize() {
-        // historicDAO = new HistoricDAO();
-        // loadMatchHistory();
+    public void initialize() throws SQLException {
+        historicDAO = new MatchHistoryDAO();
+        loadMatchHistory();
         ButtonBackToHome.setOnAction(e -> backToHome());
     }
 
-    // private void loadMatchHistory() {
-    //     List<MatchHistory> matches = historicDAO.list();
-    //     for (MatchHistory match : matches) {
-    //         HBox matchBox = createMatchBox(match);
-    //         matchesHistory.getChildren().add(matchBox);
-    //     }
-    // }
+    private void loadMatchHistory() throws SQLException {
+        List<MatchHistory> matchHistories = historicDAO.list();
+        for (MatchHistory match : matchHistories) {
+            HBox matchBox = createMatchBox(match);
+            matchesHistory.getChildren().add(matchBox);
+        }
+    }
 
-    // private HBox createMatchBox(MatchHistory match) {
-    //     // ... (método permanece o mesmo)
-    // }
+    private HBox createMatchBox(MatchHistory match) {
+        HBox matchBox = new HBox();
+        matchBox.getStyleClass().add("match-box");
+
+        VBox matchInfo = new VBox();
+        matchInfo.getStyleClass().add("match-info");
+
+        Label dateLabel = new Label("Date: " + match.getDateTime().toString());
+        Label winnerLabel = new Label("Winner: " + match.getWinner().getName());
+        VBox playersLabel = new VBox();
+        playersLabel.getChildren().add(new Label("Players: "));
+        for (Player player : match.getPlayers()) {
+            Label playerLabel = new Label(player.getName());
+            playersLabel.getChildren().add(playerLabel);
+        }
+
+        matchInfo.getChildren().addAll(dateLabel, winnerLabel, playersLabel);
+
+        return matchBox;
+    }
 
     @FXML
     private void backToHome() {
