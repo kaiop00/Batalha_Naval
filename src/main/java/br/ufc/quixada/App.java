7399f74550af8723a8907d444f37528dac91ecf4
Joao-Pedro-P-Holanda <joaopedroph.dev@gmail.com>
2024-08-25 20:49:49 -0300

    build: adding debug execution and component inspector

--- src/main/java/br/ufc/quixada/App.java
+++ src/main/java/br/ufc/quixada/App.java
@@ -1,5 +1,7 @@
 package br.ufc.quixada;
 
+import com.tangorabox.componentinspector.fx.FXComponentInspectorHandler;
+
 import br.ufc.quixada.util.SceneManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -7,12 +9,13 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage stage) {
+        FXComponentInspectorHandler.handleAll();
         stage.setTitle("Batalha Naval");
         stage.setMaximized(true);
-        
+
         SceneManager.initialize(stage);
         SceneManager.loadScene("/br/ufc/quixada/fxml/home.fxml");
-        
+
         stage.setFullScreen(true);
         stage.show();
     }
