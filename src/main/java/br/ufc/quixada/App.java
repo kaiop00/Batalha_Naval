b9ca333b3d50b664f6f3273925e13b0fdb5cb124
Pedro-Emanuel <pedroems.147@gmail.com>
2024-08-25 05:00:46 -0300

    Criando SceneManager Util. Atualizando App e Home para usarem o Manager.

--- src/main/java/br/ufc/quixada/App.java
+++ src/main/java/br/ufc/quixada/App.java
@@ -1,28 +1,20 @@
 package br.ufc.quixada;
 
+import br.ufc.quixada.util.SceneManager;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class App extends Application {
-
     @Override
-    public void start(Stage stage) throws IOException {
-        Scene scene = new Scene(loadFXML("home"), 1920, 1024);
-        stage.setScene(scene);
+    public void start(Stage stage) {
         stage.setTitle("Batalha Naval");
         stage.setMaximized(true);
+        
+        SceneManager.initialize(stage);
+        SceneManager.loadScene("/br/ufc/quixada/fxml/home.fxml");
+        
         stage.setFullScreen(true);
         stage.show();
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/br/ufc/quixada/fxml/" + fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
--- src/main/java/br/ufc/quixada/controller/HomeController.java
+++ src/main/java/br/ufc/quixada/controller/HomeController.java
@@ -1,26 +1,54 @@
 package br.ufc.quixada.controller;
 
+import br.ufc.quixada.util.SceneManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.application.Platform;
 
 public class HomeController {
     @FXML
     private StackPane rootPane;
-
+    
     @FXML
     private Button buttonStartSinglePlayer;
-
+    
     @FXML
     private Button buttonStartMultiPlayer;
-
+    
     @FXML
     private Button buttonShowHistory;
-
+    
     @FXML
     private Button buttonExit;
 
     @FXML
     private void initialize() {
+        buttonStartSinglePlayer.setOnAction(e -> handleStartSinglePlayer());
+        buttonStartMultiPlayer.setOnAction(e -> handleStartMultiPlayer());
+        buttonShowHistory.setOnAction(e -> handleShowHistory());
+        buttonExit.setOnAction(e -> handleExit());
+    }
+
+    @FXML
+    private void handleStartSinglePlayer() {
+        System.out.println("Starting Single Player game...");
+        // TODO: Implement single player game logic
+    }
+
+    @FXML
+    private void handleStartMultiPlayer() {
+        System.out.println("Starting Multi Player game...");
+        // TODO: Implement multi player game logic
+    }
+
+    @FXML
+    private void handleShowHistory() {
+        SceneManager.loadScene("/br/ufc/quixada/fxml/historic.fxml");
+    }
+
+    @FXML
+    private void handleExit() {
+        Platform.exit();
     }
 }
