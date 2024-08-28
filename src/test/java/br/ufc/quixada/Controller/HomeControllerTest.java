8db04885796a5ebb1d53349b799cbd2595737236
Francisco Paulino Arruda Filho <fpaulinofilho04@gmail.com>
2024-08-28 09:50:54 -0300

    Só fata o test do multiplayer

--- src/test/java/br/ufc/quixada/Controller/HomeControllerTest.java
+++ src/test/java/br/ufc/quixada/Controller/HomeControllerTest.java
@@ -10,23 +10,20 @@ import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit5.ApplicationTest;
 
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import br.ufc.quixada.controller.HomeController;
 import br.ufc.quixada.util.SceneManager;
 
 public class HomeControllerTest extends ApplicationTest {
 
-    private HomeController controller;
+    private Scene startingScene;
 
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/br/ufc/quixada/fxml/home.fxml"));
         Parent root = loader.load();
-        controller = loader.getController();
-        stage.setScene(new Scene(root));
+        startingScene = new Scene(root);
         stage.show();
     }
 
@@ -43,63 +40,31 @@ public class HomeControllerTest extends ApplicationTest {
 
     @Test
     public void testStartSinglePlayer() throws IOException {
-        // Simula o clique no botão "Start Single Player"
+        Parent previousRoot = startingScene.getRoot();
+        assertEquals(previousRoot, targetWindow().getScene().getRoot(), "A cena de match no modo singleplayer não foi carregada.");
         clickOn("#buttonStartSinglePlayer");
-
-        // Verifica se a cena foi trocada
-        Node matchRoot = lookup(".root").query();
-        assertNotNull(matchRoot, "A cena do jogo não foi carregada.");
+        sleep(1000);
+        // Verifica se a match scene foi carregada
+        assertNotEquals(previousRoot, targetWindow().getScene().getRoot(), "A cena de match no modo singleplayer não foi carregada.");
     }
 
     @Test
     public void testStartMultiPlayer() throws IOException {
-        // Simula o clique no botão "Start Multi Player"
+        Parent previousRoot = startingScene.getRoot();
+        assertEquals(previousRoot, targetWindow().getScene().getRoot());
         clickOn("#buttonStartMultiPlayer");
-
-        // Verifica se a cena foi trocada
-        Node matchRoot = lookup(".root").query();
-        assertNotNull(matchRoot, "A cena do jogo não foi carregada.");
+        sleep(1000);
+        // Verifica se a match scene foi carregada
+        assertNotEquals(previousRoot, targetWindow().getScene().getRoot());
     }
 
     @Test
     public void testShowHistory() {
-        // Simula o clique no botão "Show History"
+        Parent previousRoot = startingScene.getRoot();
+        assertEquals(previousRoot, targetWindow().getScene().getRoot(), "A cena de histórico de partidas não foi carregada.");
         clickOn("#buttonShowHistory");
-
-        // Verifica se a cena de histórico foi carregada
-        Node historyRoot = lookup(".root").query();
-        assertNotNull(historyRoot, "A cena de histórico não foi carregada.");
+        sleep(1000);
+        // Verifica se a cena histórico foi carregada
+        assertNotEquals(previousRoot, targetWindow().getScene().getRoot(), "A cena de histórico de partidas não foi carregada.");
     }
-
-    @Test
-    public void testStartSinglePlayerController() throws IOException {
-        // Simula o clique no botão "Start Single Player"
-        clickOn("#buttonStartSinglePlayer");
-
-        // Verifica se a função handleStartSinglePlayer foi chamada
-        // Verifica através de algum efeito esperado, por exemplo, mudança de cena ou estado
-        assertNotNull(controller);  // Verifica se o controller foi instanciado corretamente
-        // Adicione outras verificações relevantes aqui
-    }
-
-    @Test
-    public void testStartMultiPlayerController() throws IOException {
-        // Simula o clique no botão "Start Multi Player"
-        clickOn("#buttonStartMultiPlayer");
-
-        // Verifica se a função handleStartMultiPlayer foi chamada
-        assertNotNull(controller);
-        // Adicione outras verificações relevantes aqui
-    }
-
-    @Test
-    public void testShowHistoryController() {
-        // Simula o clique no botão "Show History"
-        clickOn("#buttonShowHistory");
-
-        // Verifica se a função handleShowHistory foi chamada
-        assertNotNull(controller);
-        // Adicione outras verificações relevantes aqui
-    }
-    
 }
