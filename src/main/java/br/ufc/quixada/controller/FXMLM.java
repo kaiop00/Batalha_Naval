cd73f77dd344b7857c9144df7c86be8ac29bd229
kaioportela <kaioportela@au.ufc.br>
2024-08-31 11:54:36 -0300

    Controller e testes feitos.

---
+++ src/main/java/br/ufc/quixada/controller/FXMLM.java
@@ -0,0 +1,5 @@
+package br.ufc.quixada.controller;
+
+public @interface FXMLM {
+
+}
--- src/main/java/br/ufc/quixada/model/Match.java
+++ src/main/java/br/ufc/quixada/model/Match.java
@@ -1,5 +1,6 @@
 package br.ufc.quixada.model;
 
+import javafx.scene.layout.GridPane;
 import javafx.util.Pair;
 
 import java.util.List;
@@ -40,10 +41,10 @@ public class Match {
       this.currentPlayer = currentPlayer;
   }
 
-  public void makePlay(Board board, int row, int column){
+  public boolean makePlay(int row2, int row, GridPane board){
       if(!finished){
 
-          Cell targetCell = board.getPositions()[row][column];
+          Cell targetCell = row2.getPositions()[row][board];
 
           targetCell.setRevealed(true);
 
@@ -52,17 +53,17 @@ public class Match {
 
               if(targetCell.getShip().getRemainingPieces() == 0) {
                   for (int i = 0; i < 5; i++) {
-                      if(targetCell.getShip().equals(board.getShips()[i])){
-                          board.setShipsLeft(board.getShipsLeft() - 1);
+                      if(targetCell.getShip().equals(row2.getShips()[i])){
+                          row2.setShipsLeft(row2.getShipsLeft() - 1);
                       }
                   }
 
-                  if(board.getShipsLeft() == 0){
+                  if(row2.getShipsLeft() == 0){
                       finished = true;
                   }
               }
 
-              board.getPositions()[row][column] = targetCell;
+              row2.getPositions()[row][board] = targetCell;
 
               for(Pair<Player, Board> pair : playerBoards){
                   if(!pair.getKey().equals(currentPlayer)){
@@ -93,4 +94,19 @@ public class Match {
           }
       }
   }
+
+public void end() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'end'");
+}
+
+public boolean makeMove(int row, int col) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'makeMove'");
+}
+
+public void setPlayerBoards(GridPane playerBoard, GridPane opponentBoard) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'setPlayerBoards'");
+}
 }
