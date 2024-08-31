3344b1c83f74d15edd802bead7905fac2be92240
Joao-Pedro-P-Holanda <joaopedroph.dev@gmail.com>
2024-08-30 21:28:45 -0300

    fix: fixing historic test failing on headless mode
    using SceneManager.setRoot instead of loadScene

--- src/main/java/br/ufc/quixada/controller/HistoricController.java
+++ src/main/java/br/ufc/quixada/controller/HistoricController.java
@@ -2,6 +2,8 @@ package br.ufc.quixada.controller;
 
 import br.ufc.quixada.util.SceneManager;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
@@ -9,6 +11,7 @@ import javafx.scene.layout.VBox;
 import br.ufc.quixada.model.MatchHistory;
 import br.ufc.quixada.model.Player;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -30,7 +33,13 @@ public class HistoricController {
 
     @FXML
     public void initialize() {
-        ButtonBackToHome.setOnAction(e -> backToHome());
+        ButtonBackToHome.setOnAction(e -> {
+            try {
+                backToHome();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
     }
 
     private void loadMatchHistory() throws SQLException {
@@ -51,11 +60,11 @@ public class HistoricController {
         matchBox.getStyleClass().add("match-box");
         matchBox.setMinWidth(300); // Define uma largura mínima
         matchBox.setPrefWidth(400); // Define uma largura preferida
-    
+
         VBox matchInfo = new VBox();
         matchInfo.getStyleClass().add("match-info");
         matchInfo.setMinWidth(200); // Define uma largura mínima para a VBox
-    
+
         Label dateLabel = new Label("Date: " + match.getDateTime().toString());
         Label winnerLabel = new Label("Winner: " + match.getWinner().getName());
         VBox playersLabel = new VBox();
@@ -64,15 +73,19 @@ public class HistoricController {
             Label playerLabel = new Label(player.getName());
             playersLabel.getChildren().add(playerLabel);
         }
-    
+
         matchInfo.getChildren().addAll(dateLabel, winnerLabel, playersLabel);
         matchBox.getChildren().add(matchInfo); // Adiciona matchInfo a matchBox
-    
+
         return matchBox;
     }
 
     @FXML
-    private void backToHome() {
-        SceneManager.loadScene("/br/ufc/quixada/fxml/home.fxml");
+    private void backToHome() throws IOException {
+        FXMLLoader loader = new FXMLLoader(SceneManager.class.getResource("/br/ufc/quixada/fxml/home.fxml"));
+        Parent root = loader.load();
+
+        SceneManager.setRoot(root);
+
     }
 }
